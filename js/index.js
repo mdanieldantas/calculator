ae2c01aee2393e5ab65b093f7a87430b5105976d
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-11-20 14:21:36 -0300

    adiciona manipuladores de eventos para botões da calculadora e funções auxiliares

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
+import calculate from "./calculate.js"
+import copyToClipboard from "./copyToClipboard.js"
+import { handleButtonPress, handleClear, handleTyping } from "./keyHandlers.js"
+import switchTheme from "./switchTheme.js"
 
-export default function calculate() {
-    const resultInput = document.querySelector("#result")
-    resultInput.value = "ERROR"
-    resultInput.classList.add("error")
-    const result = eval(input.value)
-    resultInput.value = result
-    resultInput.classList.remove("error")
-  }
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", handleButtonPress)
+})
+
+document.querySelector("#clear").addEventListener("click", handleClear)
+document.querySelector("#input").addEventListener("keydown", handleTyping)
+document.querySelector("#equal").addEventListener("click", calculate)
+document.querySelector("#copyToClipboard").addEventListener("click", copyToClipboard)
+document.querySelector("#themeSwitcher").addEventListener("click", switchTheme)
